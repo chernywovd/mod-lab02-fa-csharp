ad87977d50d5a33968a8611465b73db9da0d5a4a
chernywovd <136728354+chernywovd@users.noreply.github.com>
2024-05-12 17:31:00 +0300

    Чернышов И. 382007-3 lab2

--- fa/program.cs
+++ fa/program.cs
@@ -19,8 +19,9 @@ namespace fans
   }
 
 
-  public class FA1
+   public class FA1
   {
+     // Игорь Чернышов. Создание объектов состояний
      public static State a = new State()
         {
             Name = "a",
@@ -53,10 +54,12 @@ namespace fans
             IsAcceptState = false,
             Transitions = new Dictionary<char, State>()
         };
+        // Начальное состояние
         State InitialState = a;
 
         public FA1()
         {
+            // Создание переходов между состояниями
             InitialState.AddTransition('0',b);
             InitialState.AddTransition('1',e);
             b.AddTransition('0',d);
@@ -69,58 +72,73 @@ namespace fans
             e.AddTransition('1', e);
         }
 
+        // Запуск автомата на заданной строке и возврат результата
         public bool? Run(IEnumerable<char> s)
         {
             State current = InitialState;
             foreach (var c in s)
             {
+                // Переход в следующее состояние по текущему символу
                 current = current.Transitions[c];
                 if (current == null)
                     return null;
             }
+           
             return current.IsAcceptState;
         }
   }
 
-  public class FA2
-  {
-            public static State a = new State()
-            {
-               Name = "a",
-               IsAcceptState = false,
-               Transitions = new Dictionary<char, State>()
-            };
-            public static State b = new State()
-            {
-                Name = "b",
-                IsAcceptState = false,
-                Transitions = new Dictionary<char, State>()
-            };
-            public static State c = new State()
-            {
-                Name = "c",
-                IsAcceptState = true,
-                Transitions = new Dictionary<char, State>()
-            };
-            public static State d = new State()
-            {
-                Name = "d",
-                IsAcceptState = false,
-                Transitions = new Dictionary<char, State>()
-            };
 
-            State InitialState = a;
-            public FA2()
-            {
-                InitialState.AddTransition('0', d);
-                InitialState.AddTransition('1', b);
-                b.AddTransition('0', c);
-                b.AddTransition('1', InitialState);
-                c.AddTransition('0', b);
-                c.AddTransition('1', d);
-                d.AddTransition('0', a);
-                d.AddTransition('1', c);
-            }
+  public class FA2
+{
+    // Состояния автомата
+    public static State a = new State()
+    {
+        // Название состояния
+        Name = "a",
+        
+        IsAcceptState = false,
+        // Переходы из состояния
+        Transitions = new Dictionary<char, State>()
+    };
+    public static State b = new State()
+    {
+        Name = "b",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public static State c = new State()
+    {
+        Name = "c",
+        IsAcceptState = true,
+        Transitions = new Dictionary<char, State>()
+    };
+    public static State d = new State()
+    {
+        Name = "d",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    // Начальное состояние автомата
+    State InitialState = a;
+
+    // Конструктор класса
+    public FA2()
+    {
+        // Добавление переходов из начального состояния
+        InitialState.AddTransition('0', d);
+        InitialState.AddTransition('1', b);
+        // Добавление переходов из состояния b
+        b.AddTransition('0', c);
+        b.AddTransition('1', InitialState);
+        // Добавление переходов из состояния c
+        c.AddTransition('0', b);
+        c.AddTransition('1', d);
+        // Добавление переходов из состояния d
+        d.AddTransition('0', a);
+        d.AddTransition('1', c);
+    }
 
     public bool? Run(IEnumerable<char> s)
             {
@@ -137,35 +155,60 @@ namespace fans
   }
   
   public class FA3
-  {
+{
+    //Определяем состояние "a" как новое состояние.
     public static State a = new State()
-            {
-               Name = "a",
-               IsAcceptState = false,
-               Transitions = new Dictionary<char, State>()
-            };
-            public static State b = new State()
-            {
-                Name = "b",
-                IsAcceptState = false,
-                Transitions = new Dictionary<char, State>()
-            };
-            public static State c = new State()
-            {
-                Name = "c",
-                IsAcceptState = true,
-                Transitions = new Dictionary<char, State>()
-            };
-            State InitialState = a;
-            public FA3()
-            {
-                InitialState.AddTransition('0', InitialState);
-                InitialState.AddTransition('1', b);
-                b.AddTransition('0', InitialState);
-                b.AddTransition('1', c);
-                c.AddTransition('0', c);
-                c.AddTransition('1', c);
-            }
+    {
+        //Устанавливаем имя состояния как "a".
+        Name = "a",
+        //Устанавливаем свойство IsAcceptState как false
+        IsAcceptState = false,
+        //Инициализируем словарь переходов пустым словарем.
+        Transitions = new Dictionary<char, State>()
+    };
+    
+    //Определяем состояние "b" как новое состояние.
+    public static State b = new State()
+    {
+        //Устанавливаем имя состояния как "b".
+        Name = "b",
+        //Устанавливаем свойство IsAcceptState как false
+        IsAcceptState = false,
+        //Инициализируем словарь переходов пустым словарем.
+        Transitions = new Dictionary<char, State>()
+    };
+    
+    //Определяем состояние "c" как новое состояние.
+    public static State c = new State()
+    {
+        //Устанавливаем имя состояния как "c".
+        Name = "c",
+        //Устанавливаем свойство IsAcceptState как true
+        IsAcceptState = true,
+        //Инициализируем словарь переходов пустым словарем.
+        Transitions = new Dictionary<char, State>()
+    };
+    
+    //Определяем начальное состояние как "a".
+    State InitialState = a;
+    
+    //Конструктор класса FA3.
+    public FA3()
+    {
+        //Добавляем переход из начального состояния "a" в начальное состояние "a" по символу '0'.
+        InitialState.AddTransition('0', InitialState);
+        //Добавляем переход из начального состояния "a" в состояние "b" по символу '1'.
+        InitialState.AddTransition('1', b);
+        //Добавляем переход из состояния "b" в начальное состояние "a" по символу '0'.
+        b.AddTransition('0', InitialState);
+        //Добавляем переход из состояния "b" в состояние "c" по символу '1'.
+        b.AddTransition('1', c);
+        //Добавляем переход из состояния "c" в состояние "c" по символу '0'.
+        c.AddTransition('0', c);
+        //Добавляем переход из состояния "c" в состояние "c" по символу '1'.
+        c.AddTransition('1', c);
+    }
+
     public bool? Run(IEnumerable<char> s)
             {
                 State current = InitialState;
